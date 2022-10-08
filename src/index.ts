11cc9e513a530e32a2f27431d4d938b59f0afda0
NahuelAlfaro <98058039+NahuelAlfaro@users.noreply.github.com>
2022-10-07 22:24:39 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import "./components/win";
+import "./components/lose";
+import "./components/tie";
+import "./components/title";
+import "./components/button";
+import "./components/rock";
+import "./components/paper";
+import "./components/scissors";
+import "./components/score";
+
+import { state } from "./state";
+import { initRouter } from "./router";
+
+function main() {
+    const root = document.querySelector(".root");
+    initRouter(root);
+    state.init();
+}
+main();
---
+++ src/state.ts
@@ -0,0 +1,117 @@
+type Play = "piedra" | "papel" | "tijera";
+type Results = "ganaste" | "perdiste" | "empate";
+const state = {
+    data:{
+        currentGame:{
+            myPlay: "",
+            computerPlay:"",
+            results:[],
+          },
+          
+        history:{
+            player:0,
+            computer:0,
+        }
+    },
+
+    init() {
+      const data: any = localStorage.getItem("scoreData");
+      this.data.history = JSON.parse(data);
+    },
+
+    getState() {
+        return this.data;
+      },
+    
+    setState(newState) {
+        this.data = newState;
+      },
+
+      
+    playerMove(play: Play) {
+          const currentState = this.getState();
+           currentState.currentGame.myPlay = play;
+           
+           const computerMove = () => {
+           const computerPick = Math.floor(Math.random() * (3))
+           const moves = ["piedra", "papel", "tijera"]
+           const finalMove = moves[computerPick];
+           return finalMove;
+         }
+         const data = currentState as any
+        data.currentGame.computerPlay = computerMove();
+
+      },
+    // computerMove () {
+    //         const currentState = this.getState();
+    //         const computerPick = Math.floor(Math.random() * (3))
+    //         const moves = ["piedra", "papel", "tijera"]
+    //         const finalMove = moves[computerPick];
+    //         currentState.currentGame.computerPlay = finalMove;
+    //       },
+
+    whoWins(player: Play, computer: Play) {
+            const currentState = this.getState();
+            const result = currentState.currentGame.results;
+            const rockWin: boolean = player == "piedra" && computer == "tijera";
+            const paperWin: boolean = player == "papel" && computer == "piedra";
+            const scissorsWin: boolean = player == "tijera" && computer == "papel";
+            const Win = [rockWin, paperWin, scissorsWin].includes(true);
+        
+            const rockTie: boolean = player == "piedra" && computer == "piedra";
+            const paperTie: boolean = player == "papel" && computer == "papel";
+            const scissorsTie: boolean = player == "tijera" && computer == "tijera";
+            const tie = [rockTie, paperTie, scissorsTie].includes(true);
+            
+            if (Win) {
+              return result[0] = "ganaste";
+            }
+
+            else if (tie) {
+                return result[0] = "empate";
+            }
+          
+            else {
+                return result[0] = "perdiste";
+            }
+        },
+
+    pushToHistory(result: Results) {
+        const currentState = this.getState();
+        const playerScore = currentState.history.player;
+        const computerScore = currentState.history.computer;
+            
+            if (result == "ganaste") {
+                this.setState({
+                    ...currentState,
+                    history: {
+                      player: playerScore + 1,
+                      computer: computerScore,
+                    }
+                })
+                }else if (result == "perdiste") {
+                  this.setState({
+                    ...currentState,
+                    history: {
+                      player: playerScore,
+                      computer: computerScore + 1,
+                    },
+                  })
+                }
+                this.saveScore();
+              },
+
+    saveScore() {
+        const currentState = this.getState().history;
+        localStorage.setItem("scoreData", JSON.stringify(currentState));
+        },
+
+    resetScore(){
+
+        const resetScore = { player: 0, computer: 0 };
+        localStorage.setItem("scoreData", JSON.stringify(resetScore));
+            
+        }
+}
+
+export { state };
